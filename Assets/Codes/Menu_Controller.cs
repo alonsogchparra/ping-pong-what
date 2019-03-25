6b763e7e5070a36a457222f74aa22e14b1d3d376
Alonso Parra <alonsogparra@gmail.com>
2019-03-25 08:31:35 -0400

    Menu controller updated with elements that only will works on tutorial scene

--- Assets/Codes/Menu_Controller.cs
+++ Assets/Codes/Menu_Controller.cs
@@ -1,27 +1,91 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class Menu_Controller : MonoBehaviour
 {
+
+  public Text tutorialText;
+  public string[] tutorialContent;
+  public Button btnNext, btnPrev;
+  public Sprite[] sprites;
+  public Sprite btnCheck;
+  public Image imgTutorial, imgBtnCancel;
+
+  private int i = 0;
+
+  public
   // Start is called before the first frame update
   void Start()
   {
-      
+
   }
 
   // Update is called once per frame
-  void Update()
-  {
+  void Update() {
+
+    Scene currentScene = SceneManager.GetActiveScene();
+    string sceneName = currentScene.name;
+
+    if (sceneName == "Tutorial") { 
+    
+      if (tutorialText != null) {
+        tutorialText.text = tutorialContent[i];
+      }
+
+      imgTutorial.sprite = sprites[i];
+
+      if (i < tutorialContent.Length - 1) {
+
+        btnNext.interactable = true;
+        
+      } else {
+
+        btnNext.interactable = false;
       
+      }
+
+
+      if (i > 0) {
+
+        btnPrev.interactable = true;
+
+      } else {
+
+        btnPrev.interactable = false;
+      
+      }
+
+      if (i == tutorialContent.Length - 1) {
+        imgBtnCancel.sprite = btnCheck;
+      }
+
+    }
+
+
   }
 
-  public void StartGame () {
+  public void StartGame() {
     SceneManager.LoadScene(0);
   }
 
-  public void QuitGame () {
+  public void QuitGame() {
     Application.Quit();
   }
+
+  public void NextContent() {
+
+    if (i < tutorialContent.Length)
+      i++;
+
+  }
+
+  public void PreviousContent() {
+
+    if(i > 0)
+      i--;
+
+  }
+
 }
